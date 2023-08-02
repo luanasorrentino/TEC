50fa34156d5ce15b9de42c7c33a7444956243b17
luanasorrentino <luana.sorrentino@aluno.ifsp.edu.br>
2023-08-01 22:23:11 -0300

    Aula 01/08

---
+++ Exemplo1/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) 
+    throws Exception {
+
+        // declaração de variáveis
+        int number1, number2, sum;
+        number1 = 10;
+        number2 = 20;
+        sum = number1 + number2;
+
+        System.out.println("Soma = " + sum);
+    }
+}
